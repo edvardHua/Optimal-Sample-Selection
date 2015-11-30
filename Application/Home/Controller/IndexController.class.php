9e769de77eb32cae64147c7b9f700cc76e120536
Edvard <edvard_hua@live.com>
2015-11-30 17:35:57 +0800

    finish one condition

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -12,8 +12,8 @@ class IndexController extends Controller
 //        self::combination($elements, 10, 3, $result, 3);
 //        sort($result);
 //        var_dump($result);
-//        self::conditionOne(array(1,2,3,4,5),5,4,3);
-        $this->display();
+        self::conditionOne(array(1, 2, 3, 4, 5), 5, 4, 3);
+//        $this->display();
     }
 
     public function optimal()
@@ -28,31 +28,100 @@ class IndexController extends Controller
     /**
      * s=j<k
      */
-    public function conditionOne($nArray,$n, $k, $j)
+    public function conditionOne($nArray, $n, $k, $j)
     {
         $A = array(); // k elements combination
         $B = array(); // j elements combination
-        self::combination($nArray,$n,$k,$A,$k);
-        self::combination($nArray,$n,$j,$B,$j);
-
-        $countA = count($A);
+        self::combination($nArray, $n, $k, $A, $k);
+        self::combination($nArray, $n, $j, $B, $j);
+        sort($A);
+        sort($B);
 //        var_dump($A);
+//        var_dump($B);
 
+        $jInAArray = array();
         $hitMap = array(); // each $A hit map, count is the ratio
-        for($i = 0; $i < $countA; $i ++){
-            $itemArray = explode(' ',$A[$i]); // use blank to split string to array
+        foreach ($A as $key => $value) {
+            $itemArray = explode(' ', $value); // use blank to split string to array
             $itemSize = count($itemArray);
             $C = array();
-            self::combination($itemArray,$itemSize,$j,$C,$j);
+
+            self::combination($itemArray, $itemSize, $j, $C, $j);
+            $jInAArray[$key] = $C;
+
+//            var_dump($C);
+
             $countC = count($C);
-            for($m = 0; $m < $countC; $m ++){
-                $index = array_search($C[$m],$B);
-                if($index != false){ // search success
-                    $hitMap[$i][$B[$index]] = 1;
+            for ($m = 0; $m < $countC; $m++) {
+                $index = array_search($C[$m], $B);
+                if ($index !== false) { // search success, must use !== to prevent 0 case
+                    $hitMap[$key][$B[$index]] = 1;
                 }
             }
         }
-//        var_dump($hitMap);
+
+        var_dump($hitMap);
+
+
+        $result = array();
+        do{
+            $index = self::maxHit($hitMap);
+            var_dump($index);
+
+            array_push($result, $A[$index]);
+            unset($A[$index]);
+            unset($jInAArray[$index]);
+            $excludeItem = array_keys($hitMap[$index]); // transform to do diff opt
+            var_dump($excludeItem);
+
+
+            $B = array_diff($B, $excludeItem);
+
+            var_dump($B);
+
+            unset($hitMap);
+            foreach($jInAArray as $key => $value){
+                $countC = count($value);
+                for ($m = 0; $m < $countC; $m++) {
+                    $index = array_search($value[$m], $B);
+                    if ($index !== false) { // search success, must use !== to prevent 0 case
+                        $hitMap[$key][$B[$index]] = 1;
+                    }
+                }
+            }
+            var_dump($hitMap);
+        }while(!empty($B));
+
+        var_dump($result);
+    }
+
+    /**
+     * s < j = k
+     */
+    public function conditionTwo(){
+
+    }
+
+    /**
+     * s < j < k
+     */
+    public function conditionThree(){
+
+    }
+
+    /**
+     * max count in array
+     */
+    private static function maxHit($variable)
+    {
+        $index = 0;
+        $compare = count($variable[0]);
+        foreach ($variable as $key => $value) {
+            $tmp = count($value);
+            if ($tmp > $compare)
+                $index = $key;
+        }
+        return $index;
     }
 
     /**
@@ -72,10 +141,10 @@ class IndexController extends Controller
                 self::combination($candidate, $i - 1, $m - 1, $result, $M, $index);
             else {
                 $str = '';
-                for ($j = 0; $j <= $M - 1; $j++){
-                    $str .= ' '.$candidate[$index[$j]];
+                for ($j = 0; $j <= $M - 1; $j++) {
+                    $str .= ' ' . $candidate[$index[$j]];
                 }
-                $str = substr($str,1); // exclude first blank
+                $str = substr($str, 1); // exclude first blank
                 $result[] = $str;
             }
         }
