45c97d9e167a22f05383900d9f4798109142f5fb
Edvard <edvard_hua@live.com>
2015-12-01 21:06:44 +0800

    alpha

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -12,17 +12,60 @@ class IndexController extends Controller
 //        self::combination($elements, 10, 3, $result, 3);
 //        sort($result);
 //        var_dump($result);
-        self::conditionOne(array(1, 2, 3, 4, 5), 5, 4, 3);
-//        $this->display();
+//        self::conditionOne(array(1, 2, 3, 4, 5), 5, 4, 3);
+
+//        self::conditionTwo(array(1,2,3,4,5),5,4,4,3);
+        $this->display();
     }
 
     public function optimal()
     {
+//        $data = I('post.');
+//        var_dump($data);
+//        die;
+        self::func_param_empty_check(array('n','k','j','s'));
+
         $data = I('post.');
         $n = str_replace(" ", "", $data['n']);
-        // n = 1,2,3,4,5,6,7
-        // k = 5  j = 4 s = 2
         $nArray = explode(",", $n);
+        $nCount = count($nArray);
+
+        $k = $data['k'];
+        $j = $data['j'];
+        $s = $data['s'];
+
+        if($k < $j)
+            $this->error('k < j invalid input');
+        if($j < $s)
+            $this->error('j < s invalid input');
+        if($k < $s)
+            $this->error('k < s invalid input');
+
+        if($k > $j && $j == $s)
+            $result = self::conditionOne($nArray,$nCount,$k,$j);
+        else
+            $result = self::conditionTwo($nArray,$nCount,$k,$j,$s);
+        $strResult = '';
+        foreach($result as $key => $value){
+            $strResult = $strResult.'<br/>'.$value.'<br/>';
+        }
+        echo $strResult;
+//        $this->assign('result',$strResult);
+//        $this->redirect();
+    }
+
+    /**
+     * 檢查提交的參數是否爲空
+     * @param array $keys
+     */
+    protected function func_param_empty_check($keys = array(), $makeUp = array())
+    {
+        for ($i = 0; $i < count($keys); $i++) {
+            if (empty(I($keys[$i]))) {
+                !empty($makeUp) ? $this->error($makeUp[$i] . " can not be empty.") : $this->error($keys[$i] . " can not be empty.");
+                die;
+            }
+        }
     }
 
     /**
@@ -30,83 +73,148 @@ class IndexController extends Controller
      */
     public function conditionOne($nArray, $n, $k, $j)
     {
-        $A = array(); // k elements combination
-        $B = array(); // j elements combination
-        self::combination($nArray, $n, $k, $A, $k);
-        self::combination($nArray, $n, $j, $B, $j);
-        sort($A);
-        sort($B);
+        $kCombination = array(); // k elements combination
+        $jCombination = array(); // j elements combination
+        self::combination($nArray, $n, $k, $kCombination, $k);
+        self::combination($nArray, $n, $j, $jCombination, $j);
+        sort($kCombination);
+        sort($jCombination);
 //        var_dump($A);
 //        var_dump($B);
-
-        $jInAArray = array();
+        $j_from_k = array();
         $hitMap = array(); // each $A hit map, count is the ratio
-        foreach ($A as $key => $value) {
+        foreach ($kCombination as $key => $value) {
             $itemArray = explode(' ', $value); // use blank to split string to array
             $itemSize = count($itemArray);
             $C = array();
 
             self::combination($itemArray, $itemSize, $j, $C, $j);
-            $jInAArray[$key] = $C;
-
+            $j_from_k[$key] = $C;
 //            var_dump($C);
-
             $countC = count($C);
             for ($m = 0; $m < $countC; $m++) {
-                $index = array_search($C[$m], $B);
+                $index = array_search($C[$m], $jCombination);
                 if ($index !== false) { // search success, must use !== to prevent 0 case
-                    $hitMap[$key][$B[$index]] = 1;
+                    $hitMap[$key][$jCombination[$index]] = 1;
                 }
             }
         }
-
-        var_dump($hitMap);
-
-
+//        var_dump($hitMap);
         $result = array();
-        do{
+        do {
             $index = self::maxHit($hitMap);
-            var_dump($index);
-
-            array_push($result, $A[$index]);
-            unset($A[$index]);
-            unset($jInAArray[$index]);
+//            var_dump($index);
+            array_push($result, $kCombination[$index]);
+            unset($kCombination[$index]);
+            unset($j_from_k[$index]);
             $excludeItem = array_keys($hitMap[$index]); // transform to do diff opt
-            var_dump($excludeItem);
-
-
-            $B = array_diff($B, $excludeItem);
-
-            var_dump($B);
-
+//            var_dump($excludeItem);
+            $jCombination = array_diff($jCombination, $excludeItem);
+//            var_dump($B);
             unset($hitMap);
-            foreach($jInAArray as $key => $value){
+            foreach ($j_from_k as $key => $value) {
                 $countC = count($value);
                 for ($m = 0; $m < $countC; $m++) {
-                    $index = array_search($value[$m], $B);
+                    $index = array_search($value[$m], $jCombination);
                     if ($index !== false) { // search success, must use !== to prevent 0 case
-                        $hitMap[$key][$B[$index]] = 1;
+                        $hitMap[$key][$jCombination[$index]] = 1;
                     }
                 }
             }
-            var_dump($hitMap);
-        }while(!empty($B));
+//            var_dump($hitMap);
+        } while (!empty($jCombination));
 
-        var_dump($result);
+//        var_dump($result);
+        return $result;
     }
 
     /**
-     * s < j = k
+     * s < j <= k
      */
-    public function conditionTwo(){
+    public function conditionTwo($nArray, $n, $k, $j, $s)
+    {
 
-    }
+        $kCombination = array();
+        $jCombination = array();
 
-    /**
-     * s < j < k
-     */
-    public function conditionThree(){
+        self::combination($nArray, $n, $k, $kCombination, $k); // k from n
+        self::combination($nArray, $n, $j, $jCombination, $j); // j from n
 
+        var_dump($kCombination);
+        var_dump($jCombination);
+
+
+
+//        $s_from_k = array();
+        foreach ($kCombination as $skKey => $skValue) {
+            $tmp = explode(' ', $skValue);
+            $item = array();
+            self::combination($tmp, $k, $s, $item, $s);
+            $s_from_k[$skKey] = $item;
+        }
+
+//        var_dump($s_from_k);
+
+        $s_from_j = array();
+        foreach ($jCombination as $skKey => $skValue) {
+            $tmp = explode(' ', $skValue);
+            $item = array();
+            self::combination($tmp, $j, $s, $item, $s);
+            $s_from_j[$skKey] = $item;
+        }
+
+//        var_dump($s_from_j);
+
+        $matrix = array();
+        foreach ($s_from_k as $skKey => $skValue) {
+            foreach ($s_from_j as $sjKey => $sjValue) {
+                $intersection = array_intersect($skValue, $skValue); // cal intersection. if no empty set matrix($skKey , $sjKey) = 1, otherwise 0
+                if (empty($intersection)) {
+                    $matrix[$skKey][$sjKey] = 0;
+                } else {
+                    $matrix[$skKey][$sjKey] = 1;
+                    if (!isset($matrix[$skKey]['columnCount'])) // count of 1 value
+                        $matrix[$skKey]['columnCount'] = 1;
+                    else
+                        $matrix[$skKey]['columnCount']++;
+                }
+
+            }
+        }
+
+//        var_dump($matrix);
+
+        $result = array();
+        $exitCondition = count($s_from_j);
+        do{
+            // init with the first element
+            $keyValue = array_keys($skKey);
+            $maxIndex = $keyValue[0];
+            $maxColumn = $matrix[$keyValue[0]]['columnCount'];
+            // find the max columnCount
+            foreach ($s_from_k as $skKey => $skValue) {
+                if ($matrix[$skKey]['columnCount'] > $maxColumn) {
+                    $maxIndex = $skKey;
+                    $maxColumn = $matrix[$skKey]['columnCount'];
+                }
+            }
+            array_push($result, $kCombination[$maxIndex]);
+            $exitCondition -= $maxColumn;
+
+            $tmp = $matrix[$maxIndex];
+            unset($matrix[$maxIndex]);
+            // mark value 1 to 0
+            foreach ($s_from_j as $sjKey => $sjValue) {
+                foreach ($s_from_k as $skKey => $skValue) {
+                    if($tmp[$sjKey] == 1){
+                        $matrix[$skKey][$sjKey] = 0;
+                        $matrix[$skKey]['columnCount'] --;
+                    }
+                }
+            }
+        }while($exitCondition != 0);
+//        var_dump($result);
+        var_dump($s_from_k);
     }
 
     /**
